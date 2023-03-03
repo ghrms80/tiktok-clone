02edbe715f6501109dd85d4835c27a3e84afd736
jiho <ghrms80@gmail.com>
2023-03-03 22:45:29 +0900

    11.2 AnimatedList part One

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -10,6 +10,20 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(milliseconds: 500),
+      );
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,55 +33,59 @@ class _ChatsScreenState extends State<ChatsScreen> {
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(
               FontAwesomeIcons.plus,
             ),
           ),
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size10,
         ),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 30,
-              foregroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/3871193",
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: ScaleTransition(
+              scale: animation,
+              child: SizeTransition(
+                sizeFactor: animation,
+                child: ListTile(
+                  leading: const CircleAvatar(
+                    radius: 30,
+                    foregroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/3871193",
+                    ),
+                    child: Text('니꼬'),
+                  ),
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Lynn ($index)",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        "2:16 PM",
+                        style: TextStyle(
+                          fontSize: Sizes.size12,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  subtitle: const Text("Don't forget to make video"),
+                ),
               ),
-              child: Text('니꼬'),
             ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  "Lynn",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  "2:16 PM",
-                  style: TextStyle(
-                    fontSize: Sizes.size12,
-                    color: Colors.grey.shade500,
-                  ),
-                ),
-              ],
-            ),
-            subtitle: const Text("Don't forget to make video"),
-            // trailing: Text(  // 가운데 정렬
-            //   "2:16 PM",
-            //   style: TextStyle(
-            //     fontSize: Sizes.size12,
-            //     color: Colors.grey.shade500,
-            //   ),
-            // ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
