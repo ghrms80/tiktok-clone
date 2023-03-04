98762b3a82fa1a5caa3dda9c8ea745c8ce570b18
jiho <ghrms80@gmail.com>
2023-03-04 15:38:47 +0900

    12.1 CustomScrollView

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -10,6 +10,29 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          stretch: true,
+          backgroundColor: Colors.teal,
+          collapsedHeight: 80, // toolBarHeight
+          expandedHeight: 200, // full extended Height
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.blurBackground,
+              // StretchMode.fadeTitle,
+              StretchMode.zoomBackground,
+            ],
+            background: Image.asset(
+              "assets/images/placeholder.jpg",
+              fit: BoxFit.cover,
+            ),
+            title: const Text("Hello!"),
+            centerTitle: true,
+          ),
+        ),
+      ],
+    );
   }
 }
