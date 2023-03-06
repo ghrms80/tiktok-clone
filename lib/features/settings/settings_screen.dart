e6aa8870c727da36f8b04f989c110ef52957cc8c
jiho <ghrms80@gmail.com>
2023-03-06 10:37:52 +0900

    13.1 AboutListTile

---
+++ lib/features/settings/settings_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () => showAboutDialog(
+              applicationName: "TikTok Clone",
+              context: context,
+              applicationVersion: "1.0",
+              applicationLegalese: "All rights reserved. Please don't copy me.",
+            ),
+            title: const Text(
+              "About",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            subtitle: const Text("About this app...."),
+          ),
+          const AboutListTile(
+              applicationName: "TikTok Clone",
+              applicationVersion: "1.0",
+              applicationLegalese:
+                  "All rights reserved. Please don't copy me."),
+        ],
+      ),
+    );
+  }
+}
