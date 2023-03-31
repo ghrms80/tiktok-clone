3b9d56cedbf530ccee698cc48f19fd958e144faf
jiho <ghrms80@gmail.com>
2023-03-31 16:55:34 +0900

    21.2 VideoPlaybackConfigRepository

---
+++ lib/features/videos/repos/video_playback_config_repo.dart
@@ -0,0 +1,27 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class VideoPlaybackConfigRepository {
+  static const String _autoplay = "autoplay";
+  static const String _muted = "muted";
+
+  SharedPreferences _preferences;
+
+  VideoPlaybackConfigRepository(this._preferences);
+
+  Future<void> setMuted(bool value) async {
+    _preferences.setBool(_muted, value); // key에 문자열 전달
+  }
+
+  Future<void> setAutoplay(bool value) async {
+    _preferences.setBool(_autoplay, value);
+  }
+
+  bool isMuted() {
+    // null이면 false 반환
+    return _preferences.getBool(_muted) ?? false;
+  }
+
+  bool isAutoplay() {
+    return _preferences.getBool(_autoplay) ?? false;
+  }
+}
