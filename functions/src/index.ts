538e51f696c3ba79e7b82739ae040c73cd9ea80d
jiho <ghrms80@gmail.com>
2023-04-06 18:40:53 +0900

    26.5 publicUrl

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,23 +5,34 @@ admin.initializeApp();
 
 export const onVideoCreated = functions.firestore
   .document("videos/{videoId}")
-  .onCreate(async (snapshot, context) => {  // 새 영상이 생성될 때 실행
+  .onCreate(async (snapshot, context) => {
+    // 새 영상이 생성될 때 실행
     // await snapshot.ref.update({"hello":"from functions"});
-    const spawn = require('child-process-promise').spawn;
+    const spawn = require("child-process-promise").spawn;
     const video = snapshot.data();
     await spawn("ffmpeg", [
       "-i",
-      video.fileUrl,  // download video
+      video.fileUrl, // download video
       "-ss",
       "00:00:01.000", // 1 sec frame
       "-vframes",
       "1",
       "-vf", // video filter
       "scale=150:-1", // width=150, height=aspect ratio
-      `/tmp/${snapshot.id}.jpg`  // temp storage
+      `/tmp/${snapshot.id}.jpg`, // temp storage
     ]);
     const storage = admin.storage();
-    await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+    const [file, _] = await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
       destination: `thumbnails/${snapshot.id}.jpg`,
     });
+    await file.makePublic();
+    await snapshot.ref.update({ thumbnailUrl: file.publicUrl() });
+
+    const db = admin.firestore();
+    await db
+      .collection("users")
+      .doc(video.creatorUid)
+      .collection("videos")
+      .doc(snapshot.id)
+      .set({ thumbnailUrl: file.publicUrl(), videoId: snapshot.id });
   });
